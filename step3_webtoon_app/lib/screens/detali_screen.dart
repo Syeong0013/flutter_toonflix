41c0347e98bce49f97c6dffcb95a4c4c17457c71
Syeong0013 <dnwneotmxk77@hanmail.net>
2023-04-14 02:13:47 +0900

    230414_sykim_webtoon detail View

--- step3_webtoon_app/lib/screens/detali_screen.dart
+++ step3_webtoon_app/lib/screens/detali_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:step3_webtoon_app/models/webtoon_detail_model.dart';
+import 'package:step3_webtoon_app/models/webtoon_episode_model.dart';
+import 'package:step3_webtoon_app/services/api_service.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
 
   const DetailScreen({
@@ -11,55 +14,110 @@ class DetailScreen extends StatelessWidget {
   });
 
   @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  // late -> 초기화하고 프러퍼티가 있지만 생성자에서 불가능할 경우 late로 선언하고 initState()에서 초기화
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisodeModel>> episodes;
+
+  @override
+  // builid 메서드보다 먼저, 한 번만 실행되는 initState()
+  void initState() {
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatestEpisodesById(widget.id);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text(
-            title,
-            style: const TextStyle(
-              fontSize: 24,
-            ),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          widget.title,
+          style: const TextStyle(
+            fontSize: 24,
           ),
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.green,
-          elevation: 0.5,
         ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              children: [
-                const SizedBox(
-                  height: 50,
-                ),
-                Hero(
-                  tag: id,
-                  child: Container(
-                    width: 250,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        boxShadow: [
-                          BoxShadow(
-                            blurRadius: 15,
-                            offset: const Offset(10, 10),
-                            color: Colors.black.withOpacity(0.3),
-                          ),
-                        ]),
-                    clipBehavior: Clip.hardEdge,
-                    child: Image.network(
-                      thumb,
-                      headers: const {
-                        "User-Agent":
-                            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-                      },
-                    ),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        elevation: 0.5,
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Hero(
+                tag: widget.id,
+                child: Container(
+                  width: 250,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 15,
+                          offset: const Offset(10, 10),
+                          color: Colors.black.withOpacity(0.3),
+                        ),
+                      ]),
+                  clipBehavior: Clip.hardEdge,
+                  child: Image.network(
+                    widget.thumb,
+                    headers: const {
+                      "User-Agent":
+                          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                    },
                   ),
                 ),
-              ],
-            ),
-          ],
-        ));
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          FutureBuilder(
+            future: webtoon,
+            builder: (context, snapshot) {
+              // Future에 들어간 return값을 통해서 빌드를 하는데, 스냅샷 변수를 통해서 future 메소드의 상태값, 결과값 등을 알 수 있다.
+              if (snapshot.hasData) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 50,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        snapshot.data!.about,
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        '${snapshot.data!.genre} / ${snapshot.data!.age} ',
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }
+              return const Text("...");
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
